b9834af59e83771670d3c5bff05af3945cedaaf6
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-02-02 09:50:30 +0300

    add class category

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+	name = models.CharField(max_length=20)
+
+	def __str__(self):
+		return self.name
+
+
+	@staticmethod
+	def getAllCategory():
+		return Category.objects.all()
