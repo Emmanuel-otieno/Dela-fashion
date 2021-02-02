1fe68767e3d717f407da0f1007f81127482e77b4
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-02-02 10:22:40 +0300

    add class customer

--- shop/models.py
+++ shop/models.py
@@ -12,3 +12,23 @@ class Category(models.Model):
 	@staticmethod
 	def getAllCategory():
 		return Category.objects.all()
+
+
+
+class Customer(models.Model):
+	name = models.CharField(max_length=50)
+	email = models.EmailField(max_length=255)
+	phone = models.CharField(max_length=15)
+	password = models.CharField(max_length=255)
+
+	def __str__(self):
+		return self.name
+
+
+	@staticmethod
+	def emailExits(userEmail):
+		try:
+			email = Customer.objects.get(email=userEmail)
+			return email
+		except:
+			return False
