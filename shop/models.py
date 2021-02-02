f90eaa5c48d301dc3d4aeb0588807a1e6c05bf24
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-02-02 21:45:24 +0300

    add class product in models

--- shop/models.py
+++ shop/models.py
@@ -66,3 +66,32 @@ class Customer(models.Model):
 			return email
 		except:
 			return False		
+
+
+class Product(models.Model):
+	name = models.CharField(max_length=220)
+	price = models.IntegerField(default=0)
+	category = models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
+	description = models.TextField()
+	image = models.ImageField(upload_to='upload/products')
+	date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.name
+
+	# All Product get
+	@staticmethod
+	def getAllProduct():
+		return Product.objects.all()
+
+	# Filter Product By Category
+	@staticmethod
+	def getProductByFilter(category_id):
+		if category_id:
+			return Product.objects.filter(category = category_id)
+		else:
+			return Product.getAllProduct()
+
+	@staticmethod
+	def getProductById(productList):
+		return Product.objects.filter(id__in=productList)
