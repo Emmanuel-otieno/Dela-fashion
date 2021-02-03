0b1b76a18d4cbc4d985bc2bfdef8e9df68444953
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-02-03 08:50:43 +0300

    update view function

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponseRedirect
 from django.views import view
+from django.contrib.auth.hashers import check_password
 from .models import Category,Customer,Order,Product
 
 # Create your views here.
@@ -103,3 +104,31 @@ class Cart(View):
 
 		allProduct = Product.getProductById(productList)
 		return render(request,'cart.html',{"allProduct":allProduct})		
+
+class Login(View):
+	return_url = None
+
+	def get(self,request):
+		Login.return_url = request.GET.get('return_url')
+		return render(request,'login.html')
+
+	def post(self,request):
+		userData = request.POST
+		customerEmail = Customer.emailExits(userData["email"])
+		if customerEmail:
+			if check_password(userData["password"],customerEmail.password):
+				request.session["customer"] = customerEmail.id
+				if Login.return_url:
+					return HttpResponseRedirect(Login.return_url)
+				else:
+					Login.return_url = None
+					return redirect('home')
+			else:
+				return render(request,'login.html',{"userData":userData,"error":"Email or password doesn't match"})
+		else:
+			return render(request,'login.html',{"userData":userData,"error":"Email or password doesn't match"})
+
+
+def logout(request):
+	request.session.clear()
+	return redirect('home')		
