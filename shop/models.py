5be06202bb110573ba1580f5522fe78138f383ef
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-02-03 08:14:07 +0300

    edit my model class

--- shop/models.py
+++ shop/models.py
@@ -14,26 +14,6 @@ class Category(models.Model):
 		return Category.objects.all()
 
 
-
-class Customer(models.Model):
-	name = models.CharField(max_length=50)
-	email = models.EmailField(max_length=255)
-	phone = models.CharField(max_length=15)
-	password = models.CharField(max_length=255)
-
-	def __str__(self):
-		return self.name
-
-
-	@staticmethod
-	def emailExits(userEmail):
-		try:
-			email = Customer.objects.get(email=userEmail)
-			return email
-		except:
-			return False
-			
-
 class Order(models.Model):
 	product = models.ForeignKey(Product,on_delete=models.CASCADE)
 	customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
