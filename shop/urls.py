32e8fc6f2a1cab6ba0a1e7c9f1fa2a6b4765d332
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-02-03 16:07:59 +0300

    update url

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import Home,Cart,Checkout,OrderView
 from .auth import LoginCheckMiddleware,LogoutCheckMiddleware
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',Home.as_view(), name='home'),
@@ -12,3 +13,5 @@ urlpatterns = [
     path('checkout',LoginCheckMiddleware(Checkout.as_view()), name='checkout'),
     path('order',LoginCheckMiddleware(OrderView.as_view()), name='order'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
