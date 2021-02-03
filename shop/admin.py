d82e3a5ccae5bb856f3aa649aacdf4e82687f6fa
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-02-03 11:49:28 +0300

    update my admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+
+
 # Register your models here.
+class AdminProduct(admin.ModelAdmin):
+	list_display = ['id', 'name','price','category', 'date']
+
+
+class AdminCategory(admin.ModelAdmin):
+	list_display = ['name']
+
+
+admin.site.register(Product,AdminProduct)
+admin.site.register(Category,AdminCategory)
+admin.site.register(Customer)
+admin.site.register(Order)
