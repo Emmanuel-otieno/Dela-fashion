6bcc11210148dfdbadac5030b651b11492617809
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-02-03 11:47:07 +0300

    update my file url

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .views import Home,Cart,Checkout,OrderView
-
+from .auth import LoginCheckMiddleware,LogoutCheckMiddleware
 
 
 urlpatterns = [
     path('',Home.as_view(), name='home'),
     path('cart',Cart.as_view(), name='cart'),
+    path('signup',LogoutCheckMiddleware(Signup.as_view()), name='signup'),
+    path('login',LogoutCheckMiddleware(Login.as_view()), name='login'),
+    path('logout',LoginCheckMiddleware(logout), name='logout'),
+    path('checkout',LoginCheckMiddleware(Checkout.as_view()), name='checkout'),
+    path('order',LoginCheckMiddleware(OrderView.as_view()), name='order'),
 ]
