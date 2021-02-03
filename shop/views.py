3dd07277567ee73427f8d59b53e56f764b1f7430
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-02-03 08:54:34 +0300

    add sign in view function

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect,HttpResponseRedirect
 from django.views import view
-from django.contrib.auth.hashers import check_password
+from django.contrib.auth.hashers import check_password,make_password
 from .models import Category,Customer,Order,Product
 
 # Create your views here.
@@ -129,6 +129,48 @@ class Login(View):
 			return render(request,'login.html',{"userData":userData,"error":"Email or password doesn't match"})
 
 
-def logout(request):
-	request.session.clear()
-	return redirect('home')		
+    def logout(request):
+		request.session.clear()
+		return redirect('home')
+
+
+class Signup(View):
+
+	def get(self,request):
+		return render(request,'signup.html')
+			
+	def post(self,request):
+		userData = request.POST
+		# validate
+		error = self.validateData(userData)
+		if error :
+			return render(request,'signup.html',{"error":error,"userData":userData})
+		else:
+			if Customer.emailExits(userData['email']):
+				error["emailExits_error"] = "Email Already Exits"
+				return render(request,'signup.html',{"error":error,"userData":userData})
+			else:
+				customer = Customer(
+					name=userData['name'],
+					email=userData['email'],
+					phone=userData['phone'],
+					password=make_password(userData['password']),
+				)
+				customer.save()
+				return redirect('home')
+
+	# Validate form method
+	def validateData(self,userData):
+		error = {}
+		if not userData['name'] or not userData['email']  or not userData['phone']  or not userData['password'] or not userData['confirm_password']:
+			error["field_error"] = "All field must be required"
+		elif len(userData['password'])<8 and len(userData['confirm_password'])<8 :
+			error['minPass_error'] = "Password must be 8 char"
+		elif len(userData['name']) > 25 or len(userData['name']) < 3 :
+			error["name_error"] = "Name must be 3-25 charecter"
+		elif len(userData['phone']) != 11:
+			error["phoneNumber_error"] = "Phone number must be 11 charecter."
+		elif userData['password'] != userData['confirm_password']:
+			error["notMatch_error"] = "Password doesn't match"	
+
+		return error
